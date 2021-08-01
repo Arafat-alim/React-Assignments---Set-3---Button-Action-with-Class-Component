2b2bb9a7e810ef7aa6fb14543d1f9d3c00ca9fdb
ARAFAT AMAN ALIM <76907565+Arafat-alim@users.noreply.github.com>
2021-08-01 18:22:30 +0530

    Something more interesting way i fixed this code

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,35 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+   constructor(props) {
+    super(props);
+
+    this.state = {
+      isClick: true,
+      message: " ",
+    };
+  }
+	
+ onChange() {
+    this.setState({ isClick: !this.state.isClick });
+    this.setState({
+      message:
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
+    });
+  }
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+	 {this.state.isClick ? (
+          <p id="para">Hello Guys</p>
+        ) : (
+          <p id="para">{this.state.message}</p>
+        )}
+        <button onClick={() => this.onChange()}>Click Me</button>
+	
+	
     		</div>
     	);
     }
